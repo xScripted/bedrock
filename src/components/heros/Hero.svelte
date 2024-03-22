1c0f6cde89c116b8a87383b8f5c320577b0072f3
xScripted <miquel97bdn@gmail.com>
2024-03-22 14:15:49 +0100

    adssadadd

--- src/components/heros/Hero.svelte
+++ src/components/heros/Hero.svelte
@@ -3,7 +3,16 @@
 
 <style lang="scss">
   .hero {
+    text-align: center;
+    h1 {
+      font-size: 40px;
+      font-weight: bold;
+      max-width: 900px;
+    }
   }
 </style>
 
-<div class="hero">HERO!</div>
+<div class="hero">
+  <h2>Este titulo es más pequeño</h2>
+  <h1>Descubre este producto innovador para Svelte</h1>
+</div>
