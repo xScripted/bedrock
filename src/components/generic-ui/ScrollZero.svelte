9bb7010d8305188873e20ecb2998c632050c740c
Miquel Toran <138115984+miquel-toran-tramita@users.noreply.github.com>
2024-03-24 14:55:38 +0100

    Actualizar generic-ui

---
+++ src/components/generic-ui/ScrollZero.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+import Button from '@/componentes/essentials/Button.svelte'
+
+const scrollTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0
+}
+</script>
+
+<Button click="scrollTop">
+  <Svg name="arrow" />
+</Button>
