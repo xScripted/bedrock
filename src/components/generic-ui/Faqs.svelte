519c68f5244a43cf73d74ea4d870da9ef2ee8541
ViperBite03 <layitamartin@gmail.com>
2024-04-07 23:16:24 +0200

    faqs finaaal

--- src/components/generic-ui/Faqs.svelte
+++ src/components/generic-ui/Faqs.svelte
@@ -1,43 +1,50 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+
   //falta lo de los slots
 
-  interface IQuestions {
+  interface IQuestion {
     question: string
     answer: string
-    active: boolean
   }
 
-  let faqs: IQuestions[] = [
+  let faqs: IQuestion[] = [
     {
       question: 'pregunta1',
-      answer: 'respuesta1',
-      active: false,
+
+      answer:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta vero rerum quidem, laudantium explicabo corporis similique voluptatem quod amet cupiditate. Quis autem accusamus aut ab velit. Porro dolorem atque voluptatum.',
     },
     {
       question: 'pregunta2',
-      answer: 'respuesta2',
-      active: false,
+      answer: 'respuestarespuesta1respuesta1respuesta1respuesta12',
     },
     {
       question: 'pregunta3',
       answer: 'respuesta3',
-      active: false,
     },
   ]
 
-  let activeIndex = null
+  let HTMLFaqs: HTMLElement
+  let answers: NodeList
 
-  function handleClick(index: number) {
-    if (activeIndex === index) {
-      faqs[index].active = !faqs[index].active
-    } else {
-      if (activeIndex !== null) {
-        faqs[activeIndex].active = false
-      }
-      faqs[index].active = true
-      activeIndex = index
-    }
+  const handleClick = (event: any) => {
+    let selectedAnswer: HTMLElement = event.target.nextElementSibling
+
+    if (selectedAnswer.style.height != '0px') return (selectedAnswer.style.height = '0px')
+
+    answers.forEach((answer: HTMLElement) => (answer.style.height = '0px'))
+
+    selectedAnswer.style.height = selectedAnswer.getAttribute('meta-height') + 'px'
   }
+
+  onMount(() => {
+    answers = HTMLFaqs.querySelectorAll('.answer')
+    answers.forEach((answer: HTMLElement) => {
+      answer.setAttribute('meta-height', answer.offsetHeight.toString())
+      answer.style.height = '0px'
+    })
+  })
 </script>
 
 <style lang="scss">
@@ -56,26 +63,39 @@
       padding: 15px;
 
       .question {
+        transition: 0.3s ease;
         font-weight: bold;
+        text-align: left;
         cursor: pointer;
 
         &:hover {
+          transition: 0.3s ease;
           color: var(--colorBrand);
         }
+      }
+
+      .answer {
+        margin-top: 10px;
+        font-weight: lighter;
+        font-size: 15px;
+
+        overflow: hidden;
+        height: 100%;
+        text-align: justify;
+
+        transition: 0.3s ease;
       }
     }
   }
 </style>
 
-<div class="faqs">
-  {#each faqs as faq, index}
+<div class="faqs" bind:this={HTMLFaqs}>
+  {#each faqs as faq}
     <div class="pack">
-      <button class="question" on:click={() => handleClick(index)}>
+      <button class="question" on:click={handleClick}>
         {faq.question}
       </button>
-      {#if faq.active}
-        <span class="answer">{faq.answer}</span>
-      {/if}
+      <span class="answer">{faq.answer}</span>
     </div>
   {/each}
 </div>
