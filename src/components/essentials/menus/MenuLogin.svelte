ed054b5f3b5df3bce4595a8a82eee4cd95712f27
ViperBite03 <layitamartin@gmail.com>
2024-04-10 02:19:54 +0200

    sad

--- src/components/essentials/menus/MenuLogin.svelte
+++ src/components/essentials/menus/MenuLogin.svelte
@@ -13,6 +13,12 @@
     { title: 'Osteopatía', href: '' },
     { title: 'Esclerosis', href: '' },
   ]
+
+  let options: ILink[] = [
+    { title: 'Honolulu', href: '' },
+    { title: 'Honolulu', href: '' },
+    { title: 'Honolulu', href: '' },
+  ]
 </script>
 
 <style lang="scss">
@@ -53,7 +59,7 @@
         height: 50px;
         width: 50px;
         border-radius: 50px;
-        overflow: hidden;
+        position: relative;
 
         box-shadow:
           3px 3px 10px var(--colorBackground),
@@ -61,6 +67,46 @@
 
         img {
           height: 100%;
+        }
+
+        .account {
+          position: absolute;
+          width: fit-content;
+          height: fit-content;
+          min-width: 130px;
+          padding: 10px 15px;
+
+          background-color: var(--colorBase2);
+          border-radius: 8px;
+
+          top: 65px;
+          right: 0px;
+          display: none;
+
+          .options {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 10px;
+            margin-right: 5px;
+            text-align: end;
+          }
+
+          .divisor {
+            width: 100%;
+            height: 1px;
+            background-color: var(--colorBorder);
+            margin: 5px 0;
+          }
+
+          .signOut {
+            font-size: 13px;
+          }
+        }
+
+        &:hover {
+          .account {
+            display: block;
+          }
         }
       }
 
@@ -94,8 +140,17 @@
     <button class="notifications">
       <Svg name="bellOff" height="30" width="30" />
     </button>
-    <div class="profilePicture">
+    <button class="profilePicture">
       <img src={profilePicture} alt="" />
-    </div>
+      <div class="account">
+        <div class="options">
+          {#each options as option}
+            <a href={option.href} class="option">{option.title}</a>
+          {/each}
+        </div>
+        <div class="divisor" />
+        <button class="signOut">Cerrar sesión</button>
+      </div>
+    </button>
   </div>
 </div>
