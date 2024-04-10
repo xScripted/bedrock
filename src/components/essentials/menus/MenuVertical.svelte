3cf4aa767d21baecdc856ae16ed7f60bfa76ac26
ViperBite03 <layitamartin@gmail.com>
2024-04-10 17:39:04 +0100

    menu vertical acabado

--- src/components/essentials/menus/MenuVertical.svelte
+++ src/components/essentials/menus/MenuVertical.svelte
@@ -18,35 +18,66 @@
   ]
 
   let menuItemsDown: IMenuItem[] = [
-    { item: 'Perfil', icon: 'instagram', href: '' },
-    { item: 'Ajustes', icon: 'instagram', href: '' },
+    { item: 'Perfil', icon: 'person', href: '' },
+    { item: 'Ajustes', icon: 'settings', href: '' },
   ]
+
+  let expanded: boolean = true
 </script>
 
 <style lang="scss">
-  .menu {
+  .menu-container {
+    transition: 0.3s ease;
+
     height: 100dvh;
-    width: fit-content;
-    min-width: 175px;
+    width: 175px;
     background-color: var(--colorBase);
-    padding: 20px;
+    padding: 10px;
     position: absolute;
     left: 0;
 
     display: flex;
     flex-direction: column;
     gap: 50px;
+    overflow: hidden;
+
+    &.active {
+      width: 66px;
+      transition: 0.3s ease;
+
+      .logo {
+        transition: 0.3s ease;
+        width: 46px !important;
+      }
+
+      .brand {
+        display: none;
+      }
+
+      .item {
+        transition: 0.3s ease;
+        opacity: 0;
+      }
+
+      .expand {
+        transform: rotate(180deg);
+        transition: 0.5s ease;
+      }
+    }
 
     .details {
       height: fit-content;
       width: 100%;
       display: flex;
-      justify-content: space-around;
+      gap: 15px;
 
       .logo {
+        transition: 0.5s ease;
+
         height: 60px;
         width: 60px;
         border-radius: 50px;
+        flex-shrink: 0;
 
         /*overflow: hidden;
         box-shadow:
@@ -55,11 +86,11 @@
 
         img {
           border-radius: 8px;
-          height: 100%;
+          width: 100%;
         }
       }
 
-      .d-container {
+      .brand {
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -81,12 +112,12 @@
       justify-content: space-between;
       height: 100%;
 
-      .menuItems {
+      .menu-items {
         display: flex;
         flex-direction: column;
         gap: 10px;
 
-        .menuItem {
+        .menu-item {
           display: flex;
           align-items: center;
           gap: 8px;
@@ -95,6 +126,10 @@
           font-weight: 300;
           transition: 0.3s ease;
 
+          :global(*) {
+            flex-shrink: 0;
+          }
+
           &:hover {
             transition: 0.3s ease;
             box-shadow:
@@ -102,37 +137,49 @@
               -3px -3px 10px var(--colorBase2);
           }
         }
+
+        .expand {
+          transition: 0.5s ease;
+
+          width: fit-content;
+          padding: 8px;
+          display: flex;
+          align-items: center;
+        }
       }
     }
   }
 </style>
 
-<div class="menu">
+<div class="menu-container" class:active={!expanded}>
   <div class="details">
     <div class="logo"><img src={logo} alt="" /></div>
-    <div class="d-container">
+    <div class="brand">
       <span class="company">Tramita</span>
       <div class="panel">{panel}</div>
     </div>
   </div>
 
   <div class="menus">
-    <div class="menuItems">
+    <div class="menu-items">
       {#each menuItemsUp as menuItem}
-        <a class="menuItem" href={menuItem.href}>
+        <a class="menu-item" href={menuItem.href}>
           <Svg name={menuItem.icon} height="30" width="30" />
           <span class="item">{menuItem.item}</span>
         </a>
       {/each}
     </div>
 
-    <div class="menuItems">
+    <div class="menu-items">
       {#each menuItemsDown as menuItem}
-        <a class="menuItem" href={menuItem.href}>
+        <a class="menu-item" href={menuItem.href}>
           <Svg name={menuItem.icon} height="30" width="30" />
           <span class="item">{menuItem.item}</span>
         </a>
       {/each}
+      <button class="expand" on:click={() => (expanded = !expanded)} title={expanded ? 'Minimizar' : 'Expandir'}>
+        <Svg name="arrow2" height="30" width="30" />
+      </button>
     </div>
   </div>
 </div>
