65ed00a673f5dd043e2f0845b2c1679af32ca0db
ViperBite03 <layitamartin@gmail.com>
2024-04-15 16:15:07 +0200

    asdas

--- src/components/generic-ui/SliderX.svelte
+++ src/components/generic-ui/SliderX.svelte
@@ -7,10 +7,19 @@
   let HTMLslides: HTMLElement
   let index: number = 0
 
+  let disableNext: boolean = false
+  let disablePrev: boolean = true
+  let onMounted: boolean = false
+
   $: if (HTMLslides) HTMLslides.style.transform = `translateX(${itemWidth * index}px)`
+  $: disablePrev = index >= 0
+  $: disableNext = onMounted && -itemWidth < window.innerWidth - HTMLslides.getBoundingClientRect().right
 
   const previous = () => {
-    if (index < 0) index++
+    if (index < 0) {
+      index++
+      disableNext = false
+    }
   }
 
   const next = () => {
@@ -22,6 +31,7 @@
   onMount(() => {
     const nItems: number = HTMLslides.children.length
     HTMLslides.style.width = `${nItems * itemWidth}px`
+    onMounted = true
   })
 </script>
 
@@ -56,6 +66,10 @@
           transform: translateX(1px);
         }
 
+        &.disabled {
+          opacity: 0.2;
+        }
+
         &.left {
           transform: rotate(180deg);
         }
@@ -83,11 +97,11 @@
     </div>
 
     <div class="arrows">
-      <button class="arrow left" on:click={() => previous()}>
+      <button class="arrow left" class:disabled={disablePrev} on:click={previous}>
         <Svg name="arrowSlim" width="25" height="25" />
       </button>
 
-      <button class="arrow right" on:click={() => next()}>
+      <button class="arrow right" class:disabled={disableNext} on:click={next}>
         <Svg name="arrowSlim" width="25" height="25" />
       </button>
     </div>
