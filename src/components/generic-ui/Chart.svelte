902bf6e7cc235f79ea3723692b4d142148d24200
Scripted <miquel97bdn@gmail.com>
2024-09-24 12:44:24 +0200

    ChartDone!

--- src/components/generic-ui/Chart.svelte
+++ src/components/generic-ui/Chart.svelte
@@ -1,7 +1,50 @@
 <script lang="ts">
+  import Chart from 'chart.js/auto'
+  import { onMount } from 'svelte'
+
+  let HTMLChart: HTMLCanvasElement
+
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ]
+
+  onMount(() => {
+    new Chart(HTMLChart, {
+      type: 'bar',
+      data: {
+        labels: data.map((row) => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map((row) => row.count),
+          },
+        ],
+      },
+      options: {
+        animation: {
+          duration: 6000, // Duración de la animación (en milisegundos)
+          easing: 'easeOutBounce', // Tipo de transición (puedes probar otros como 'linear', 'easeInOutQuad', etc.)
+          onProgress: function (animation) {
+            // Función de callback para cada frame de la animación
+            console.log('Progreso:', animation.currentStep / animation.numSteps)
+          },
+          onComplete: function (animation) {
+            // Función de callback cuando termina la animación
+            console.log('¡Animación completada!')
+          },
+        },
+      },
+    })
+  })
 </script>
 
 <style lang="scss">
 </style>
 
-hols
+<canvas id="chart" bind:this={HTMLChart}></canvas>
